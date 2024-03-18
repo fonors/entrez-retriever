29b0bf6aa15c49bfce9359e48e5081e533245a05
Fonos <thea_martian@proton.me>
2024-03-18 12:13:34 +0000

    Added search query function

--- entrez-retriever.py
+++ entrez-retriever.py
@@ -16,3 +16,21 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
+import argparse, requests
+from sys import stderr, exit
+
+parser = argparse.ArgumentParser(
+                    prog = 'Entrez Retriever',
+                    description = 'Use the Entrez API to perform NCBI queries for sequences.')
+
+parser.add_argument("-db", "--database", help="Database to parse in")
+parser.add_argument("-t", "--term", help="Search term for the query")
+args = parser.parse_args()
+
+def search_query():
+    """
+    Performs a search query in the NCBI databases. Receives the database and the search term, along with any additional parameters.
+    Uses the 'history' feature.
+    """
+    search_params = {'db': args.database, 'term': args.term, 'usehistory': 'y'}
+    server_request = requests.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi/', params=search_params)
