77f336b52a3b92590118a2cb7e85f2c6d48ec092
fonors <thea_martian@proton.me>
2024-03-27 12:57:11 +0000

    Added documentation and made the script runnable

--- entrez-retriever.py
+++ entrez-retriever.py
@@ -16,31 +16,34 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
-import argparse, requests
+import argparse, requests, xml.etree.ElementTree as ET
 
 parser = argparse.ArgumentParser(
                     prog = 'Entrez Retriever',
                     description = 'Use the Entrez API to perform NCBI queries for sequences.')
 
-def search_args(parser):
-    parser.add_argument("-db", "--database", help="")
-    parser.add_argument("-t", "--term", help="")
-    parser.add_argument("-w", "--webenv", help="")
-    parser.add_argument("-q", "--querykey", help="")
-    parser.add_argument("-Rs", "--retstart", help="")
-    parser.add_argument("-Rm", "--retmax", help="")
-    parser.add_argument("-Rt", "--rettype", help="")
-    parser.add_argument("-Rmd", "--retmode", help="")
-    parser.add_argument("-s", "--sort", help="")
-    parser.add_argument("-f", "--field", help="")
-    parser.add_argument("-i", "--idtype", help="")
-    parser.add_argument("-dt", "--datetype", help="")
-    parser.add_argument("-rd", "--reldate", help="")
-    parser.add_argument("-md", "--mindate", help="")
-    parser.add_argument("-Md", "--maxdate", help="")
+def search_args(argparser):
+    """
+    Gets the arguments the user defined when running the script and returns them.
+    """
+    argparser.add_argument("-db", "--database", help="Database to search in.")
+    argparser.add_argument("-t", "--term", help="Entrez text query.")
+    argparser.add_argument("-w", "--webenv", help="Web environment string returned from a previous ESearch, EPost or ELink call. ")
+    argparser.add_argument("-q", "--querykey", help="Integer query key returned by a previous ESearch, EPost or ELink call.")
+    argparser.add_argument("-Rs", "--retstart", help="Sequential index of the first UID in the retrieved set to be shown in the XML output.")
+    argparser.add_argument("-Rm", "--retmax", help="Total number of UIDs from the retrieved set to be shown in the XML output .")
+    argparser.add_argument("-Rt", "--rettype", help="Retrieval type. There are two allowed values for ESearch: 'uilist' (default), which displays the standard XML output, and 'count', which displays only the <Count> tag.")
+    argparser.add_argument("-Rmd", "--retmode", help="Retrieval type. Determines the format of the returned output. The default value is 'xml' for ESearch XML, but 'json' is also supported to return output in JSON format.")
+    argparser.add_argument("-s", "--sort", help="Specifies the method used to sort UIDs in the ESearch output.")
+    argparser.add_argument("-f", "--field", help="Search field. If used, the entire search term will be limited to the specified Entrez field.")
+    argparser.add_argument("-i", "--idtype", help="Specifies the type of identifier to return for sequence databases (nuccore, popset, protein).")
+    argparser.add_argument("-dt", "--datetype", help="Type of date used to limit a search.")
+    argparser.add_argument("-rd", "--reldate", help="When reldate is set to an integer n, the search returns only those items that have a date specified by datetype within the last n days.")
+    argparser.add_argument("-md", "--mindate", help="Minimum date used to limit a search result by the date specified by datetype. Must be used together with maxdate to specify an arbitrary date range.")
+    argparser.add_argument("-Md", "--maxdate", help="Maximum date used to limit a search result by the date specified by datetype. Must be used together with mindate to specify an arbitrary date range.")
 
-    args = parser.parse_args()
-    return args
+    search_args = argparser.parse_args()
+    return search_args
 
 def search_query(search_args):
     """
@@ -70,6 +73,11 @@ def search_query(search_args):
     return server_request
 
 def seq_fetcher(query_results, og_search_args):
+    """
+    Uses the search_query() function to get the desired sequences, then it uses the 'history' feature to parse all the desired sequences and
+    returns them in FASTA format.
+    Outputs to STDOUT.
+    """
     query_xml_elemtree = ET.fromstring(query_results.text)
 
     query_key = query_xml_elemtree.find("QueryKey").text
@@ -83,3 +91,8 @@ def seq_fetcher(query_results, og_search_args):
 
     server_request = requests.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi', params=fetch_params)
     print(server_request.text)
+
+if __name__ == "__main__":
+    user_args = search_args(parser)
+    server_info = search_query(user_args)
+    seq_fetcher(server_info, user_args)
